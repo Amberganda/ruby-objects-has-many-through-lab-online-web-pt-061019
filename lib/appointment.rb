ace464259a6f9ee77fbbd584eee206cb5335b015
Amber Taranto <ambzzzz@gmail.com>
2019-08-06 04:20:17 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,5 +1,14 @@
 class Appointment
 
+    @@all = []
 
+    attr_accessor :appointment
+
+    def initialize(date, patient, doctor)
+        @date = date
+        @patient = patient
+        @doctor = doctor
+        @@all << self
+    end
 
 end
