0e8a65af338a5800a651ed08991525cdd3e57343
Amber Taranto <ambzzzz@gmail.com>
2019-08-06 04:35:51 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -24,7 +24,7 @@ class Doctor
     end
 
     def patients
-        Appointment.all.map do |appointment|
+        self.appointments.map do |appointment|
             appointment.patient
         end
     end
